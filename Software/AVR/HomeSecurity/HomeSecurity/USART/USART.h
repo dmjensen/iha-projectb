79b29fec76c0509b6f2c80cbffff85190641f35d
madsvonqualen@gmail.com <madsvonqualen@gmail.com@03037290-6654-1fc7-fe71-60be2f6569b4>
2011-08-15 17:53:13 +0000

    Fixed bug in main()

--- Software/AVR/HomeSecurity/HomeSecurity/USART/USART.h
+++ Software/AVR/HomeSecurity/HomeSecurity/USART/USART.h
@@ -5,17 +5,17 @@
 
 #define UBRRVAL ((F_CPU/(BAUDRATE*16UL))-1)
 
-//States:
-#define S_START 0xA0	//Idle
-#define S_ID 0xA1		//Waiting for device ID
-#define S_EVENT_NO 0xA2	//Waiting for number of events
-#define S_TYPE 0xA3		//Waiting for event type
-#define S_HOUR 0xA4		//Waiting for event hour
-#define S_MIN 0xA5		//Waiting for event minute
-
-//Commands:
-#define C_Reset 0xB0	//Resets the state machine to S_START
-#define C_CONF 0xB1		//Received when in S_START and PC wants to initiate configuration of a device
+////States:
+//#define S_START 0xA0	//Idle
+//#define S_ID 0xA1		//Waiting for device ID
+//#define S_EVENT_NO 0xA2	//Waiting for number of events
+//#define S_TYPE 0xA3		//Waiting for event type
+//#define S_HOUR 0xA4		//Waiting for event hour
+//#define S_MIN 0xA5		//Waiting for event minute
+//
+////Commands:
+//#define C_Reset 0xB0	//Resets the state machine to S_START
+//#define C_CONF 0xB1		//Received when in S_START and PC wants to initiate configuration of a device
 
 void USART_Init(void);
 
